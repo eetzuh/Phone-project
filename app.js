24068072c447bbd2a3c347317fb9cd37766ace9d
prdjed <100497741+prdjed@users.noreply.github.com>
2022-12-19 18:41:45 +0100

    Handling click on letters buttons

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
-let start = Date.now();
-let count = -1;
+let message = " ";
+let count = 0;
+let previous = "";
+let time = 0;
 
-function click(letter) {
-  let now = Date.now();
-  count++;
-  let text = "";
-  console.log(now - start);
-  let textbox = document.getElementsByClassName("display-text");
-  if (now - start < 6) {
-    text += letter.charAt(count);
+function clickingOnButton(letters) {
+  let currentTime = Date.now();
+
+  if (currentTime - time < 500 && previous == letters) {
+    count++;
+    if (count == letters.length) {
+      count = 0;
+    }
+    message = message.slice(0, -1) + `${letters.charAt(count)}`;
   } else {
-    count = -1;
-    text += letter.charAt(0);
+    count = 0;
+    message += letters.charAt(count);
   }
-  start = now;
-  let message=document.createTextNode(text)
-  textbox.appendChild(text)
-  // return text
-}
 
-console.log(click("hello"));
-console.log(click("hello"));
-console.log(click("hello"));
+  document.getElementById("textual").innerHTML = message;
+  time = currentTime;
+  previous = letters;
+}
