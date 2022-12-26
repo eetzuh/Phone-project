859eb8fbde3d19e9f96b92a3a01c042095b6c834
eetzuh <irenakandic9@gmail.com>
2022-12-26 13:00:08 +0100

    Added call feautre

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ let count = 0;
 let previous = "";
 let time = 0;
 let upper = false;
+let del=false;
 let screen=""
 
 
 function clickingOnButton(letters) {
   let currentTime = Date.now();
   message=document.getElementById("textual").innerHTML
-  if (currentTime - time < 500 && previous == letters) {
+  if (currentTime - time < 900 && previous == letters) {
     count++;
     if (count == letters.length) {
       count = 0;
@@ -34,13 +35,11 @@ function clickingOnButton(letters) {
       message +=letters.substring(count,count+1)
     }
   }
-
   document.getElementById("textual").innerHTML = message;
   time = currentTime;
   previous = letters;
   screen="messages"
 }
-
 
 function eraser(){
   document.getElementById("textual").innerHTML=document.getElementById("textual").innerHTML.slice(0, document.getElementById("textual").innerHTML.length-1)
@@ -54,10 +53,32 @@ function submit(){
 }
 
 function goToMenu(){
+  memory= document.getElementById("textual").innerHTML
   document.getElementById("menu").innerHTML="MENU"
+  document.getElementById("sendEmail").innerHTML="&#9679 Send email"
   document.getElementById("textual").innerHTML=textual.parentNode.removeChild(textual)
   screen="menu"
-  console.log(message)
+  console.log(memory)
+}
+function goBack(){
+  if(screen=="menu"){
+  document.getElementById("menu").innerHTML=""
+  document.getElementById("sendEmail").innerHTML=""
+  document.getElementById("shownScreen").appendChild(document.createElement('p')).setAttribute("id","textual")
+  document.getElementById("textual").innerHTML=memory
+  }
+}
+
+function call(){
+  let regexForPlus=/^[+]\d+$/
+  let regexForZero=/^[0]\d+$/
+  if(regexForPlus.test(document.getElementById("textual").innerHTML)==true){
+    let num=document.getElementById("textual").innerHTML.substring(1,document.getElementById("textual").innerHTML.length)
+    return window.open(`viber://chat?number=${num}`)
+  }else if(regexForZero.test(document.getElementById("textual").innerHTML)==true){
+    let num="382"+document.getElementById("textual").innerHTML.substring(1,document.getElementById("textual").innerHTML.length)
+    return open(`viber://chat?number=${num}`)
+  }
 }
 
 function clock(){
